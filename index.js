bb573ac2443f0702407148fa3bb37eaf739e472e
fattah <fattahmuhyiddeen@gmail.com>
2021-07-14 18:23:28 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { useHistory } from 'react-router-dom';
 
-export default (key) => {
+export default (key, defaultValue) => {
   const history = useHistory();
 
   const q = new URLSearchParams(window.location.search);
-  const value = q.get(key);
+  const value = q.get(key) || defaultValue;
   const setValue = v => {
     const queries = new URLSearchParams(window.location.search);
     queries.set(key, v);
